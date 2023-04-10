3ecccddaa568490e917b2ec274d84f182fe00d5b
Elizabeth Wilson <zilqnova@gmail.com>
2023-04-10 17:56:42 -0400

    fixed parameters, implementing balance

--- calculate-business.lua
+++ calculate-business.lua
@@ -9,36 +9,29 @@ function print_help(error_message)
 	os.exit()
 end
 
+function match_whole_pattern(input, pattern)
+	return string.find(input, "%f[%w-]" .. pattern .. "%f[^%w-]")
+end
+
 --weeks need to be converted to days for the purpose of calculations
 if #arg == 0 or string.match(table.unpack(arg), "-h") then print_help() end
 local params = {}
-for i = 1, #arg, 2 do --if an argument without a following value is added, the amount to increment should be variable instead of 2
-	--[[
-	if arg[i] == "-c" then
-		if arg[i+1] == nil or arg[i+1] == "-f" or arg[i+1] == "-w" then io.write("ERROR: No value supplied to -c\n"); print_help() end
-		params.weekly_cost = tonumber(arg[i+1])
-		if type(params.weekly_cost) ~= "number" then io.write("ERROR: -c must be supplied a number\n"); print_help() end
-	elseif arg[i] == "-w" then
-		if arg[i+1] == nil or arg[i+1] == "-f" or arg[i+1] == "-c" then io.write("ERROR: No value supplied to -w\n"); print_help() end
-		params.weeks = tonumber(arg[i+1])
-		if type(params.weeks) ~= "number" then io.write("ERROR: -w must be supplied an integer\n"); print_help() end
-	elseif arg[i] == "-f" then
-		if arg[i+1] == nil or arg[i+1] == "-c" or arg[i+1] == "-w" then io.write("ERROR: No value supplied to -f\n"); print_help() end
-		params.file = arg[i+1] 
-	end
-	--]]
+for i = 1, #arg, 2 do
 	params[arg[i]] = arg[i+1]
 end
 
-local flags_paired = {"-b", "-c", "-d", "-f", "-w",} --if an argument without a following value is ever added, the flag will not appear in this table
-for k, v in pairs(params) do
+local flags_paired = {"-b", "-c", "-d", "-f", "-w", "--divisor"} --if an argument without a following value is ever added, the flag will not appear in this table
+for k, v in pairs(params) do --ensure that all arguments have values and are correct
+	local match_unrecognized = true
+	local match_novalue = false
 	for _, w in ipairs(flags_paired) do
-		if string.match(w, k) == nil then
-			print_help("Argument not recognized")
-		elseif string.match(w, v) ~= nil then
-			print_help("An argument is missing a value")
-		end
+		print(k, v, _, w)
+		if match_whole_pattern(w, k) ~= nil then match_unrecognized = false end
+		if match_whole_pattern(w, v) ~= nil then match_novalue = true end
 	end
+	print(match_unrecognized, match_novalue)
+	if match_unrecognized == true then print_help("Argument not recognized") end
+	if match_novalue == true then print_help("An argument is missing a value") end
 end
 
 if params["-b"] == nil then
@@ -53,10 +46,12 @@ elseif params["-w"] ~= nil and params["-d"] ~= nil then
 	print_help("-d and -w are incompatible with one another")
 end
 
+local balance = params["-b"]
 local weekly_cost = params["-c"]
 local weeks = params["-w"] or 1
-local file = params["-f"]
 local days = params["-d"] or (tonumber(weeks) * 7)
+local file = params["-f"]
+local divisor = params["--divisor"] or 1
 
 function file_exists(file)
 	local f = io.open(file, "rb")
@@ -83,16 +78,16 @@ function roll_bonus()
 	--track # of days up to 30
 	local roll_count = 0
 	return function ()
-		if roll_count < 30 then roll_count = rollcount + 1 end
+		if roll_count < 30 then roll_count = roll_count + 1 end
 		return roll_count --closure to keep track of bonus up to 30
 	end
 end
 
 function roll_days()
 	local rolls = {}
-	local bonus = roll_bonus()
+	local bonus = roll_bonus() --arithmetic error
 	for i = 1, days do
-		rolls[i] = roll(100) + bonus 
+		rolls[i] = roll(100) + bonus() 
 	end
 	return rolls
 end
@@ -101,7 +96,7 @@ function line_days()
 	local lines = lines_from(file)
 	local bonus = roll_bonus()
 	for i, v in ipairs(lines) do
-		lines[i] = tonumber(v) + bonus
+		lines[i] = tonumber(v) + bonus()
 	end
 	return lines
 end
@@ -112,10 +107,10 @@ function check_rolls()
 	if file ~= nil then
 		local values = line_days()
 	else
-		values = roll_days()
+		local values = roll_days()
 	end
 
-	for _, value in ipairs(values) do
+	for _, value in ipairs(values) do --table from dmg p.128
 		if value >= 1 and value <= 20 then
 			sum = sum + (weekly_cost * -1.5)
 		elseif value >= 21 and value <= 30 then
@@ -131,6 +126,7 @@ function check_rolls()
 		elseif value >= 91 then
 			sum = sum + ((roll(10, 3) * 5)/10) --3d10 * 5sp
 		end
+		balance = balance + sum
 		::continue::
 		print("Current iteration: " .. _ .. "\nCurrent value: " .. value .. "\nCurrent sum: " .. sum)
 	end
