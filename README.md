c861abe01fb1b9c472a74ba275d36d730f3bd595
Elizabeth Wilson <zilqnova@gmail.com>
2023-04-11 22:17:57 -0400

    finished penalty, bonus tracking

--- README.md
+++ README.md
@@ -3,6 +3,7 @@ This is a collection of scripts to automate the boring parts of Dungeons & Drago
 
 ## Requirements
  - Lua (5.4)
+ - lua\_cliargs
 
 ## Current Scripts
  - calculate-business.lua | This script allows you to provide a number of weeks (or a file of rolls with one roll per line) and a weekly cost of upkeep for a business to calculate profits (or losses!)
--- calculate-business.lua
+++ calculate-business.lua
@@ -1,6 +1,8 @@
 --A script to calculate total business earnings (and losses) in D&D 5e
 math.randomseed(os.time())
 
+--just replace all of this using lua_cliargs :/
+--also make functions local
 function print_help(error_message)
 	if error_message ~= nil then
 		io.write("ERROR: ", error_message, "\n")
@@ -13,23 +15,28 @@ function match_whole_pattern(input, pattern)
 	return string.find(input, "%f[%w-]" .. pattern .. "%f[^%w-]")
 end
 
---weeks need to be converted to days for the purpose of calculations
 if #arg == 0 or string.match(table.unpack(arg), "-h") then print_help() end
+local flags_unpaired = {"-v",}
+local flags_paired = {"-b", "-c", "-d", "-f", "-w", "--divisor"}
 local params = {}
 for i = 1, #arg, 2 do
+	local unpaired = false
+	for _, v in ipairs(flags_unpaired) do
+		if match_whole_pattern(arg[i], v) ~= nil then unpaired = true end
+	end
+	if unpaired == true then
+		table.insert(arg, i+1, true)
+	end
 	params[arg[i]] = arg[i+1]
 end
 
-local flags_paired = {"-b", "-c", "-d", "-f", "-w", "--divisor"} --if an argument without a following value is ever added, the flag will not appear in this table
 for k, v in pairs(params) do --ensure that all arguments have values and are correct
 	local match_unrecognized = true
 	local match_novalue = false
 	for _, w in ipairs(flags_paired) do
-		print(k, v, _, w)
 		if match_whole_pattern(w, k) ~= nil then match_unrecognized = false end
 		if match_whole_pattern(w, v) ~= nil then match_novalue = true end
 	end
-	print(match_unrecognized, match_novalue)
 	if match_unrecognized == true then print_help("Argument not recognized") end
 	if match_novalue == true then print_help("An argument is missing a value") end
 end
@@ -46,12 +53,13 @@ elseif params["-w"] ~= nil and params["-d"] ~= nil then
 	print_help("-d and -w are incompatible with one another")
 end
 
-local balance = params["-b"]
-local weekly_cost = params["-c"]
+local balance = tonumber(params["-b"])
+local weekly_cost = tonumber(params["-c"])
 local weeks = params["-w"] or 1
-local days = params["-d"] or (tonumber(weeks) * 7)
+local days = tonumber(params["-d"]) or (tonumber(weeks) * 7)
 local file = params["-f"]
-local divisor = params["--divisor"] or 1
+local divisor = tonumber(params["--divisor"]) or 1
+local verbose = params["-v"] or false
 
 function file_exists(file)
 	local f = io.open(file, "rb")
@@ -85,9 +93,14 @@ end
 
 function debt_penalty() --use an instance of this closure in check_rolls() to dynamically update a new total bonus
 	local penalty_count = 0
-	return function ()
-		penalty_count = penalty_count + 10
-		return penalty_count
+	return function (reset)
+		if reset == false then
+			penalty_count = 0
+			reset = nil
+		else
+			penalty_count = penalty_count + 10
+			return penalty_count
+		end
 	end
 end
 
@@ -109,53 +122,54 @@ end
 
 function check_rolls()
 	local sum = 0
+	local cost_profit = 0
 	local values = {}
 	if file ~= nil then
-		local values = line_days()
+		values = line_days()
 	else
-		local values = roll_days()
+		values = roll_days()
 	end
+	if verbose == true then io.write("Values: ", table.unpack(values), "\n") end
 	local bonus = roll_bonus()
 	local penalty = debt_penalty()
-	for _, value in ipairs(values) do --table from dmg p.128
-		value = value + bonus()
-		if balance <= 0 then value = value - penalty() end
+	for _, v in ipairs(values) do --table from dmg p.129
+		value = v + bonus()
+		if balance <= 0 then value = value - penalty() else penalty(false) end --reset penalty to roll if debt can be paid (balance goes back above 0)
+		if value <= 0 then value = 1 end
 		if value >= 1 and value <= 20 then
-			sum = sum + (weekly_cost * -1.5)
+			cost_profit = (weekly_cost * -1.5)
+			sum = sum + cost_profit
 		elseif value >= 21 and value <= 30 then
-			sum = sum + (weekly_cost * -1)
+			cost_profit = (weekly_cost * -1)
+			sum = sum + cost_profit
 		elseif value >= 31 and value <= 40 then
-			sum = sum + (weekly_cost * -0.5)
+			cost_profit = (weekly_cost * -0.5)
+			sum = sum + cost_profit 
 		elseif value >= 41 and value <= 60 then
 			goto continue
 		elseif value >= 61 and value <= 80 then
-			sum = sum + ((roll(6) * 5)/10)
+			cost_profit = ((roll(6) * 5)/divisor)
+			sum = sum + cost_profit
 		elseif value >= 81 and value <= 90 then
-			sum = sum + ((roll(8, 2) * 5)/10)
+			cost_profit = ((roll(8, 2) * 5)/divisor)
+			sum = sum + cost_profit 
 		elseif value >= 91 then
-			sum = sum + ((roll(10, 3) * 5)/10)
+			cost_profit = ((roll(10, 3) * 5)/divisor)
+			sum = sum + cost_profit 
 		end
-		balance = balance + sum
+		balance = balance + cost_profit
 		::continue::
-		print("Current iteration: " .. _ .. "\nCurrent value: " .. value .. "\nCurrent sum: " .. sum)
+		if verbose == true then io.write("Current iteration: ", _, "\tCurrent value: ", value, "\tCurrent sum: ", sum, "\tCurrent balance: ", balance, "\tCurrent cost or profit: ", cost_profit, "\n") end
 	end
-	return sum
+	return sum, balance
 end
 
---[[
-local lines = lines_from(file)
-for k, v in ipairs(lines) do
-	print(k, v)
-end
-os.exit()
---]]
-
-local total = check_rolls()
+local total, total_balance = check_rolls()
 
 if total < 0 then
-	io.write("This business has lost ", total, "gp", "\n")
+	io.write("This business has lost ", total, "gp. Your new balance is ", total_balance, "gp.\n")
 	os.exit()
 else
-	io.write("This business has made ", total, "gp", "\n")
+	io.write("This business has made ", total, "gp. Your new balance is ", total_balance, "gp.\n")
 	os.exit()
 end
